dfa28df4955aa66994665f033ef924eb2257a516
TharinduSK <tskumarage@gmail.com>
2019-11-03 03:23:41 -0700

    Package creation: Text vectorization

---
+++ text_vectorization/word_embed.py
@@ -0,0 +1,9 @@
+"""
+Authors : Kumarage Tharindu & Paras Sheth
+Class : CSE 472
+Organization : ASU CIDSE
+Project : SMM Project 2
+Task : Text preprocessor : Cleaning and pre-processing news data
+
+"""
+
